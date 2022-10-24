549a693353c9f21d7cf67855107bc4030c03c0b8
Lydia <lydiaezekiel48@gmail.com>
2022-10-24 16:46:54 -0700

    my first line of C

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,4 +1,4 @@
-fndef _MAIN_H_
+#ifndef _MAIN_H_
 #define _MAIN_H_
 
 void reset_to_98(int *n);
@@ -13,5 +13,4 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
-
 #endif
