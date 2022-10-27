ab7440d78f13e93ac676729af0578f63a08df8f0
Lydia <lydiaezekiel48@gmail.com>
2022-10-27 10:56:50 -0700

    my first line of C

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main - print 0123456789
+ *Return: Always 0 (success)
+ */
+int main(void)
+{
+int num = 0;
+
+while (num <= 9)
+{
+printf("%i", num);
+num++;
+}
+putchar('\n');
+return (0);
+}
+
